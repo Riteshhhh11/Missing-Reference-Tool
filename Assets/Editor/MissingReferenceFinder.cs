2c0e5289c838d62cd5c896e83c04ce35aecbd103
Riteshhhh <riteshranjan785@gmail.com>
2026-06-12 02:51:48 +0530

    Added a dynamic list to store text

--- Assets/Editor/MissingReferenceFinder.cs
+++ Assets/Editor/MissingReferenceFinder.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
-using UnityEditor; // Let's you access libraries for editor scripting
+using UnityEditor;
+using System.Collections.Generic; // Let's you access libraries for editor scripting
 
 public class MissingReferenceEditor : EditorWindow {
+
+    private List<string> results = new List<string>();
+    private Vector2 scrollPos;
     [MenuItem("Tools/Missing Reference Finder")] // Adds a menu item to the Unity Editor under "Tools" called "Missing Reference Finder"
     public static void OpenWindow() { // Because it's static unity can run this method directly from the menu
         GetWindow<MissingReferenceEditor>("Missing Reference Finder"); // Opens the editor window with the title "Missing Reference Finder"
@@ -10,7 +14,22 @@ public class MissingReferenceEditor : EditorWindow {
     private void OnGUI() // This method is called to draw the GUI of the editor window
     {
         if(GUILayout.Button("Scan Project")) {
-            Debug.Log("Button Clickeds");
+            results.Clear();
+            ScanProject();
         }
+
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField($"Results: {results.Count}", EditorStyles.boldLabel);
+
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+        foreach (var r in results) {
+            EditorGUILayout.HelpBox(r, MessageType.Warning);
+        }
+
+        EditorGUILayout.EndScrollView();
+    }
+
+    private void ScanProject() {
+        results.Add("Logic to be added yet");
     }
 }
