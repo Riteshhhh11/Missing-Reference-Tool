17edb1ad82fa4fc25deed781eb0fa5482b3792f8
Riteshhhh <riteshranjan785@gmail.com>
2026-06-13 03:04:48 +0530

    Added scanning logic for the project, scans for the scenes and objects and also for the children components

--- Assets/Editor/MissingReferenceFinder.cs
+++ Assets/Editor/MissingReferenceFinder.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEditor;
-using System.Collections.Generic; // Let's you access libraries for editor scripting
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class MissingReferenceEditor : EditorWindow {
 
@@ -29,7 +31,65 @@ public class MissingReferenceEditor : EditorWindow {
         EditorGUILayout.EndScrollView();
     }
 
-    private void ScanProject() {
-        results.Add("Logic to be added yet");
+    private void ScanProject() { //ScanProject has one job find every asset that could contain a missing reference; two assets that can be mainly missing are .prefab and .unity
+        string[] allPaths = AssetDatabase.GetAllAssetPaths(); // Get all asset paths in the project
+        foreach (string path in allPaths) {
+            if (!path.StartsWith("Assets/")) {
+                continue; 
+            }
+            if (path.EndsWith(".prefab")) {
+                GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path); // Load the prefab at the given path
+
+                if (prefab != null) {
+                    CheckObject(prefab, path);
+                }
+            }
+            else if (path.EndsWith(".unity")) {
+                ScanScene(path);
+            }
+        }
     }
+
+    private void ScanScene(string scenePath) {
+        Scene scene = EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Additive); // Open the scene additively so we can check it without affecting the currently open scene
+        foreach (GameObject root in scene.GetRootGameObjects()) {
+            CheckObject(root, scenePath);
+        }
+        EditorSceneManager.CloseScene(scene, true); // Close the scene after checking it
+    }
+
+    private void CheckObject(GameObject gameobject, string assetPath) {
+        Component[] components = gameobject.GetComponents<Component>(); // Get all components attached to the gameobject
+        foreach (Component component in components) {
+            // component itsell can be null if it's script is missing
+            if (component == null) {
+                results.Add($"[Script is missing]\nAsset: {assetPath}\nGameObject: {gameobject.name}");
+                continue;
+            }
+            // wrap the components in a seralized object so that we can iterate through it's serialized references
+            SerializedObject serializedObject = new SerializedObject(component); // Create a serialized object to access the properties of the gameobject
+            SerializedProperty property = serializedObject.GetIterator(); // Get an iterator to go through all properties of the gameobject
+
+            while (property.NextVisible(true))
+            {
+                // Key Check: null value but non-zero instance ID indicates a missing reference
+                if (property.propertyType == SerializedPropertyType.ObjectReference)
+                {
+                    if (property.objectReferenceValue == null && property.objectReferenceInstanceIDValue != 0)
+                    {
+                        results.Add($"[Missing Reference]\n" +
+                                    $"Asset: {assetPath}\n" +
+                                    $"GameObject: {gameobject.name}\n" +
+                                    $"Component: {component.GetType().Name}\n" +
+                                    $"property: {property.displayName}"
+                                    );
+                    }
+                }
+            }
+        }
+        foreach (Transform child in gameobject.transform) {
+            CheckObject(child.gameObject, assetPath);
+        }
+    }
+        
 }
---
+++ Assets/TestRef.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class TestRef : MonoBehaviour
+{
+    public GameObject testObj; // This is a public variable of type GameObject that can be assigned in the Unity Editor. It will be used to test if the Missing Reference Finder can detect missing references.
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
